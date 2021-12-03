fd0163f1f7e304747fe759ae33e38933dd28ffea
tsendee <tsendayush@nashatech.com>
2021-12-03 18:02:00 +0800

    vuex

---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="product-list-two">
+      <h2>Product List Two</h2>
+      <ul>
+          <li v-for="product in saleProducts" :key="product">
+              <span class="name">{{ product.name}}</span>
+              <span class="price">{{ product.price}}</span>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+        products(){
+            return this.$store.state.products
+        },
+        saleProducts(){
+            return this.$store.getters.saleProducts;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
